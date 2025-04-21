ce05f92aa23b51e79e894d554e8ce75a6d2c922a
TianyuanHuuu <160672802+TianyuanHuuu@users.noreply.github.com>
2025-04-21 16:49:12 -0400

    Update listener.py

--- listener.py
+++ listener.py
@@ -1,39 +1,24 @@
 from web3 import Web3
 from web3.providers.rpc import HTTPProvider
-from web3.middleware import ExtraDataToPOAMiddleware #Necessary for POA chains
-from pathlib import Path
+from web3.middleware import ExtraDataToPOAMiddleware
 import json
 from datetime import datetime
 import pandas as pd
 
 
 def scan_blocks(chain, start_block, end_block, contract_address, eventfile='deposit_logs.csv'):
-    """
-    chain - string (Either 'bsc' or 'avax')
-    start_block - integer first block to scan
-    end_block - integer last block to scan
-    contract_address - the address of the deployed contract
-
-	This function reads "Deposit" events from the specified contract, 
-	and writes information about the events to the file "deposit_logs.csv"
-    """
     if chain == 'avax':
-        api_url = f"https://api.avax-test.network/ext/bc/C/rpc" #AVAX C-chain testnet
-
-    if chain == 'bsc':
-        api_url = f"https://data-seed-prebsc-1-s1.binance.org:8545/" #BSC testnet
-
-    if chain in ['avax','bsc']:
-        w3 = Web3(Web3.HTTPProvider(api_url))
-        # inject the poa compatibility middleware to the innermost layer
-        w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
+        api_url = "https://api.avax-test.network/ext/bc/C/rpc"
+    elif chain == 'bsc':
+        api_url = "https://data-seed-prebsc-1-s1.binance.org:8545/"
     else:
-        w3 = Web3(Web3.HTTPProvider(api_url))
+        raise ValueError("Unsupported chain")
+
+    w3 = Web3(Web3.HTTPProvider(api_url))
+    w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
 
     DEPOSIT_ABI = json.loads('[ { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "token", "type": "address" }, { "indexed": true, "internalType": "address", "name": "recipient", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "Deposit", "type": "event" }]')
     contract = w3.eth.contract(address=contract_address, abi=DEPOSIT_ABI)
-
-    arg_filter = {}
 
     if start_block == "latest":
         start_block = w3.eth.get_block_number()
@@ -41,23 +26,36 @@ def scan_blocks(chain, start_block, end_block, contract_address, eventfile='depo
         end_block = w3.eth.get_block_number()
 
     if end_block < start_block:
-        print( f"Error end_block < start_block!" )
-        print( f"end_block = {end_block}" )
-        print( f"start_block = {start_block}" )
+        raise ValueError("end_block cannot be less than start_block")
 
-    if start_block == end_block:
-        print( f"Scanning block {start_block} on {chain}" )
-    else:
-        print( f"Scanning blocks {start_block} - {end_block} on {chain}" )
+    events_data = []
+
+    def process_events(events):
+        for evt in events:
+            row = {
+                'chain': chain,
+                'token': evt.args['token'],
+                'recipient': evt.args['recipient'],
+                'amount': evt.args['amount'],
+                'transactionHash': evt.transactionHash.hex(),
+                'address': evt.address,
+                'date': datetime.utcnow().strftime("%m/%d/%Y %H:%M:%S")
+            }
+            events_data.append(row)
 
     if end_block - start_block < 30:
-        event_filter = contract.events.Deposit.create_filter(from_block=start_block,to_block=end_block,argument_filters=arg_filter)
+        event_filter = contract.events.Deposit.create_filter(fromBlock=start_block, toBlock=end_block)
         events = event_filter.get_all_entries()
-        #print( f"Got {len(events)} entries for block {block_num}" )
-        # TODO YOUR CODE HERE
+        process_events(events)
     else:
-        for block_num in range(start_block,end_block+1):
-            event_filter = contract.events.Deposit.create_filter(from_block=block_num,to_block=block_num,argument_filters=arg_filter)
+        for block_num in range(start_block, end_block + 1):
+            event_filter = contract.events.Deposit.create_filter(fromBlock=block_num, toBlock=block_num)
             events = event_filter.get_all_entries()
-            #print( f"Got {len(events)} entries for block {block_num}" )
-            # TODO YOUR CODE HERE
+            process_events(events)
+
+    if events_data:
+        df = pd.DataFrame(events_data)
+        df.to_csv(eventfile, index=False)
+    else:
+        columns = ['chain', 'token', 'recipient', 'amount', 'transactionHash', 'address', 'date']
+        pd.DataFrame(columns=columns).to_csv(eventfile, index=False)
